26707812a1bb125dc2849fcdb2a5993f8dc2f1e3
HuMart <manya01@protonmail.com>
2019-09-13 12:53:35 -0400

    fix and working

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -10,10 +10,9 @@ def count_th(word):
     #         count += 1    
     
     # return count
-    for i in range(0, len(word)-1):
         if len(word) < 2:
             return 0        
-        elif word[i] == "t" and word[i+1] == "h":
+        elif word[0] == "t" and word[1] == "h":
             return 1 + count_th(word[1:])
         else:
             return count_th(word[1:])
